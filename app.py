2120712b436468abd4b2887cd68a19fa2a6bdfcf
Victor <victor.dmaina@gmail.com>
2024-08-16 09:47:32 +0300

    add backend

--- app.py
+++ app.py
@@ -33,4 +33,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    
+
