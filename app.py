df8d765d7b4ee53da47b750a560293888e430d55
Victor <victor.dmaina@gmail.com>
2024-08-17 22:05:12 +0300

    update files

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_migrate import Migrate
 from models import db
 from config import Config
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -34,6 +33,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-    
-
+    app.run(debug=True)
